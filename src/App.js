bd6c2c0f2ea33b8f3617c881af4adebb11bcbe4e
systematixrahul <rahul.yadav@systematixindia.com>
2021-12-21 18:18:31 +0530

    first blank commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+
+function App() {
+  return;
+  <BrowserRouter>
+    <div className="App">Hello</div>;
+  </BrowserRouter>;
+}
+
+export default App;
